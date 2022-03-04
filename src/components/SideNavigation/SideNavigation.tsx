0d21590bd3e5a0de21b7c861bc0714eae495c3d1
abrightkite <phy5397@gmial.com>
2022-03-05 03:24:07 +0900

    solve complict

--- src/components/SideNavigation/SideNavigation.tsx
+++ src/components/SideNavigation/SideNavigation.tsx
@@ -1,14 +1,8 @@
 import React from "react";
-<<<<<<< HEAD
 import "./css/sideNavigation.css";
 
 const SideNavigation = () => {
   return <div className="side-navigation-wrap">Side Navigation</div>;
-=======
-
-const SideNavigation = () => {
-  return <div>Side Navigation</div>;
->>>>>>> master
 };
 
 export default SideNavigation;
