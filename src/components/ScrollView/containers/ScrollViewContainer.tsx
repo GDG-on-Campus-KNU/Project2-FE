7f87c3990b673c2039978c6e4c61adff77782239
bh2980 <bh2980@naver.com>
2022-05-30 15:26:26 +0900

    fix(Merge Error): imcompleted Category feature

--- src/components/ScrollView/containers/ScrollViewContainer.tsx
+++ src/components/ScrollView/containers/ScrollViewContainer.tsx
@@ -45,7 +45,6 @@ const ScrollViewContainer = ({
   const onload = useCallback(() => {
     console.log(token);
   }, [token]);
-
   const closePopUp = useCallback(() => {
     __hidePopUpFromHooks();
   }, []);
@@ -54,24 +53,18 @@ const ScrollViewContainer = ({
     __showPopUpFromHooks(<WritePopUpContainer closePopUp={closePopUp} />);
   }, []);
 
-  const addItemList = useCallback(
-    (blocks: getBlockType[]) => {
-      setItemList([...itemList, ...blocks]);
-    },
-    [itemList]
-  );
+  const addItemList = (blocks: getBlockType[]) => {
+    setItemList([...itemList, ...blocks]);
+  };
 
-  const intersecting = useCallback(
-    async ([entry]: IntersectionObserverEntry[]) => {
-      if (entry.isIntersecting) {
-        const blocks = await getBlocks();
-        setLoading(true);
-        addItemList(blocks);
-        setLoading(false);
-      }
-    },
-    []
-  );
+  const intersecting = async ([entry]: IntersectionObserverEntry[]) => {
+    if (entry.isIntersecting) {
+      const blocks = await getBlocks();
+      setLoading(true);
+      addItemList(blocks);
+      setLoading(false);
+    }
+  };
 
   const observer = new IntersectionObserver(intersecting, { threshold: 0.4 });
 
