ef2fef6da4d9d54da925e34fdfc172f003f47373
bh2980 <bh2980@naver.com>
2022-06-22 02:25:17 +0900

    feat(InfiniteScroll) : add InfiniteScroll conponent

---
+++ src/components/common/InfiniteScroll/InfiniteScroll.tsx
@@ -0,0 +1,80 @@
+import { useState, useEffect, ReactElement, LegacyRef } from "react";
+
+type Props = {
+  itemList: ReactElement[];
+  addItemList: () => Promise<void>;
+  end: boolean;
+  spinner: ReactElement;
+};
+
+const InfiniteScroll = ({ itemList, addItemList, end, spinner }: Props) => {
+  const [target, setTarget] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const Spinner = () => {
+    return spinner;
+  };
+
+  const targetStyle = {
+    width: "100%",
+    height: "1px",
+  };
+
+  const callback = async (
+    [entry]: { isIntersecting: boolean }[],
+    observer: IntersectionObserver
+  ) => {
+    console.log("intersecting");
+    if (!target) return;
+
+    if (entry.isIntersecting) {
+      setLoading(true);
+      await addItemList();
+      setLoading(false);
+    }
+  };
+
+  const observer = new IntersectionObserver(callback, { threshold: 0.4 });
+
+  useEffect(() => {
+    if (!target) return;
+
+    observer.observe(target);
+
+    return () => observer && observer.disconnect();
+  }, [target]);
+
+  return (
+    <>
+      {itemList.map((item, index) => {
+        const Item = () => {
+          return item;
+        };
+        return <Item key={index} />;
+      })}
+      {loading && <Spinner />}
+      {!end && !loading && (
+        <div
+          style={targetStyle}
+          ref={setTarget as LegacyRef<HTMLDivElement>}
+        ></div>
+      )}
+    </>
+  );
+};
+
+const DefaultSpinner = () => {
+  const spinnerStyle = {
+    width: "100%",
+    height: "50px",
+    textAlign: "center",
+  } as React.CSSProperties;
+
+  return <div style={spinnerStyle}>Loading...</div>;
+};
+
+InfiniteScroll.defaultProps = {
+  spinner: <DefaultSpinner />,
+};
+
+export default InfiniteScroll;
